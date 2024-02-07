2c7885c12a61621d5d4a4ca5524985c6f298e40c
Maria McParland <78078583+maria-mcparland@users.noreply.github.com>
2024-02-07 10:54:45 +0000

    Fix up generate certs test

--- python/sample-authentication-code/tests/generateCertificates_test.py
+++ python/sample-authentication-code/tests/generateCertificates_test.py
@@ -5,26 +5,25 @@ import unittest
 import os
 import subprocess
 from dotenv import load_dotenv
+import os
 
 # Load environment variables from .env file
 load_dotenv()
+
+COUNTRY_NAME=os.getenv('COUNTRY_NAME')
+STATE_OR_PROVINCE_NAME=os.getenv('STATE_OR_PROVINCE_NAME')
+LOCALITY_NAME=os.getenv('LOCALITY_NAME')
+ORGANIZATION_NAME=os.getenv('ORGANIZATION_NAME')
+COMMON_NAME=os.getenv('COMMON_NAME')
+DNS_NAME=os.getenv('DNS_NAME')
 
 # Import the function to be tested
 from generateCertificates import generate_self_signed_cert
 
 class TestGenerateCertificates(unittest.TestCase):
-    def setUp(self):
-        # Set up any necessary environment variables
-        os.environ['COUNTRY_NAME'] = 'UK'
-        os.environ['STATE_OR_PROVINCE_NAME'] = 'Scotland'
-        os.environ['LOCALITY_NAME'] = 'Glasgow'
-        os.environ['ORGANIZATION_NAME'] = 'My Organization'
-        os.environ['COMMON_NAME'] = 'localhost'
-        os.environ['DNS_NAME'] = 'localhost'
-
     def test_generate_self_signed_cert(self):
         # Call the function to generate the certificate
-        generate_self_signed_cert()
+        generate_self_signed_cert(COUNTRY_NAME, STATE_OR_PROVINCE_NAME, LOCALITY_NAME, ORGANIZATION_NAME, COMMON_NAME, DNS_NAME)
 
         # Check if the certificate and private key files are generated
         self.assertTrue(os.path.exists("private_key.pem"))
