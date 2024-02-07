231685b85f4dac9e79150db4462e37fa70ee9da8
Maria McParland <78078583+maria-mcparland@users.noreply.github.com>
2024-02-07 10:50:16 +0000

    fIX TESTS

--- python/sample-authentication-code/getAccessToken.py
+++ python/sample-authentication-code/getAccessToken.py
@@ -1,16 +1,9 @@
 import requests
 '''
 This snippet highlights how to obtain an OAuth access token using Python.
-You will provide your CLIENT_ID, CLIENT_SECRET and ACCESS_TOKEN_URL in a .env file.
+You will provide your CLIENT_ID, CLIENT_SECRET and ACCESS_TOKEN_URL.
 You can obtain these values following this guide: https://developer.payments.jpmorgan.com/quick-start
 '''
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
-CLIENT_ID=os.getenv('CLIENT_ID')
-CLIENT_SECRET=os.getenv('CLIENT_SECRET')
-ACCESS_TOKEN_URL=os.getenv('ACCESS_TOKEN_URL')
 
 
 def get_access_token(url, client_id, client_secret):
@@ -20,7 +13,3 @@ def get_access_token(url, client_id, client_secret):
         auth=(client_id, client_secret),
     )
     return response.json()["access_token"]
-
-
-access_token = get_access_token(ACCESS_TOKEN_URL,CLIENT_ID,CLIENT_SECRET)
-print(access_token)
