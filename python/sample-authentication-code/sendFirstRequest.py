249e95bea662a9c1548005eb908ad23dea9cb589
Maria McParland <78078583+maria-mcparland@users.noreply.github.com>
2024-02-19 16:13:36 +0000

    Adding python oauth sample server

--- python/sample-authentication-code/sendFirstRequest.py
+++ python/sample-authentication-code/sendFirstRequest.py
@@ -5,7 +5,7 @@ def send_first_request(payload, url, accessToken):
     headers = {
     "Content-Type": "application/json",
     "Accept": "application/json",
-    "Authorization": accessToken    
+    "Authorization": "Bearer " + accessToken    
     }
     response = requests.post(url, json=payload, headers=headers)
     return response.json()
